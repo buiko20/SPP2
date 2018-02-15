73f2dc8013f6ddb87462e9b810b9d0525dc6851c
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-02-15 08:06:22 +0300

    * Update src.dao.DAOFactory
    - Delete old comments
    
    * Rename from AspirantAccountDAO to DAO

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,18 +1,15 @@
 package dao;
 
-// Note: you can use only domain.
-
 import dao.impl.DbAspirantAccountDAO;
 
 /**
  * Represents a simple DAO factory.
  */
 public class DAOFactory {
-    // This field must be static. (delete this)
+
     private static final DAOFactory factory = new DAOFactory();
 
-    // Do not make myDAO and other fields like this static. (delete this)
-    private final AspirantAccountDAO AspirantAccountDAO = new DbAspirantAccountDAO();
+    private final DAO AspirantAccountDAO = new DbAspirantAccountDAO();
 
     private DAOFactory() {
     }
@@ -26,12 +23,10 @@ public class DAOFactory {
     }
 
     /**
-     * Returns an instance of the {@link AspirantAccountDAO} in singleton scope.
-     * @return instance of the {@link AspirantAccountDAO}
+     * Returns an instance of the {@link DAO} in singleton scope.
+     * @return instance of the {@link DAO}
      */
-    public AspirantAccountDAO getAspirantAccountDAO() {
+    public DAO getAspirantAccountDAO() {
         return AspirantAccountDAO;
     }
-
-    // TODO: refactor code, add DAO.
 }

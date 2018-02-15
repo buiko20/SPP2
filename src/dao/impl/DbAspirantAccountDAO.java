c4d68936f0ede5022c86a6f2fba95d01aa309c9c
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-02-15 08:18:03 +0300

    * Update src.dao.impl.DbAspirantAccountDAO
    + Add connection private field
    + Add setConnection private method
    + Add a implementation of class methods
    
    * Rename from src.dao.AspirantAccountDAO to DAO

--- src/dao/impl/DbAspirantAccountDAO.java
+++ src/dao/impl/DbAspirantAccountDAO.java
@@ -1,29 +1,121 @@
 package dao.impl;
 
-import dao.AspirantAccountDAO;
+import dao.DAO;
 import dao.exception.DAOException;
 import domain.AspirantAccount;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DbAspirantAccountDAO implements AspirantAccountDAO{
+public class DbAspirantAccountDAO implements DAO<AspirantAccount>{
+
+    private Connection connection = null;
+
+    private void setConnection() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        connection =  DriverManager.getConnection("jdbc:mysql://localhost/jobs_db?user=root&password=root");
+    }
+
     public List<AspirantAccount> getAll() throws DAOException {
-        return null;
+        ArrayList<AspirantAccount> aspirantAccounts = new ArrayList<>();
+        try{
+            setConnection();
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Aspirant");
+
+            while(resultSet.next()){
+                AspirantAccount aspirantAccount = new AspirantAccount(
+                        resultSet.getInt("id"),
+                        resultSet.getString("Email"),
+                        resultSet.getString("Password"),
+                        null
+                );
+                aspirantAccounts.add(aspirantAccount);
+            }
+
+            connection.close();
+            return aspirantAccounts;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public AspirantAccount getById(int id) throws DAOException {
-        return null;
+        try{
+            setConnection();
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT FROM Aspirant WHERE id=" + id);
+
+            AspirantAccount aspirantAccount = null;
+            if(resultSet.next()){
+                aspirantAccount = new AspirantAccount(
+                        resultSet.getInt("id"),
+                        resultSet.getString("Email"),
+                        resultSet.getString("Password"),
+                        null
+                );
+            }
+
+            connection.close();
+            return aspirantAccount;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public boolean update(AspirantAccount aspirantAccount) throws DAOException {
-        return false;
+        try{
+            setConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute("update Aspirant set Email='" +
+                    aspirantAccount.getEmail() +
+                    "', Password='" + aspirantAccount.getPassword() +
+                    "' where id=" + aspirantAccount.getId());
+
+            connection.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean delete(int id) throws DAOException {
-        return false;
+        try {
+            setConnection();
+
+            Statement statement = connection.createStatement();
+            statement.execute("DELETE FROM Aspirant WHERE id=" + id);
+
+            connection.close();
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean create(AspirantAccount aspirantAccount) throws DAOException {
-        return false;
+        try {
+            setConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute("INSERT INTO Aspirant (Email, Password, Aspirant_data_id)" +
+                    "VALUES ('" + aspirantAccount.getEmail() + "', '" +
+                    aspirantAccount.getPassword() + "', NULL)");
+
+            connection.close();
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }

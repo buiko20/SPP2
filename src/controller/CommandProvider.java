73ece21c574cfbedb690acd091b51820ebddb9fb
Anastasiya <galtsova98@gmail.com>
2018-03-13 22:27:16 +0300

    Update src/controller/CommandProvider.java
    + Add new commands to the list of commands

--- src/controller/CommandProvider.java
+++ src/controller/CommandProvider.java
@@ -2,6 +2,12 @@ package controller;
 
 import controller.command.Command;
 import controller.command.impl.*;
+import controller.command.impl.GetCompanyCommand;
+import controller.command.impl.InvitationCommands.GetInvitationCommand;
+import controller.command.impl.InvitationCommands.GetInvitationListForAspirantCommand;
+import controller.command.impl.InvitationCommands.SendInvitationCommand;
+import controller.command.impl.JobVacancyCommands.*;
+import controller.command.impl.ResumeCommands.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,13 +24,30 @@ public class CommandProvider {
     public CommandProvider() {
         addCommand(new RegisterAspirantCommand(), "Register");
         addCommand(new LoginAspirantCommand(), "Login");
+
+        addCommand(new GetResumeListForAspirantCommand(), "GetResumeListForAspirant");
+        addCommand(new GetResumeListForHRManagerCommand(), "GetResumeListForHRManager");
+
         addCommand(new CreateResumeCommand(), "CreateResume");
-        addCommand(new GetResumeListCommand(), "GetResumeList");
         addCommand(new GetResumeCommand(), "GetResume");
         addCommand(new UpdateResumeCommand(), "UpdateResume");
         addCommand(new DeleteResumeCommand(), "DeleteResume");
         addCommand(new UpdateResumeDateCommand(), "UpdateResumeDate");
         addCommand(new GetResumeViewListCommand(), "GetResumeViewList");
+
+        addCommand(new GetJobVacancyListForHRManagerCommand(), "GetJobVacancyListForHRManager");
+        addCommand(new GetJobVacancyListForAspirant(), "GetJobVacancyListForAspirant");
+
+        addCommand(new CreateJobVacancyCommand(), "CreateJobVacancy");
+        addCommand(new GetJobVacancyCommand(), "GetJobVacancy");
+
+        addCommand(new GetCompanyCommand(), "GetCompany");
+
+        addCommand(new GetInvitationCommand(), "GetInvitationCommand");
+        addCommand(new SendInvitationCommand(), "SendInvitationCommand");
+
+        //addCommand(new GetInvitationListForHRManagerCommand(), "GetInvitationListForHRManager");
+        addCommand(new GetInvitationListForAspirantCommand(), "GetInvitationListForAspirant");
     }
 
     /**
